5b5fa2ffd571f7064fe2795a488061ed53f2e59b
ajeetkesherwani <111976456+ajeetkesherwani@users.noreply.github.com>
2023-08-07 22:51:36 +0530

    update search faeture

--- packages/Devi/UI/src/Resources/views/front/search.blade.php
+++ packages/Devi/UI/src/Resources/views/front/search.blade.php
@@ -3,26 +3,19 @@
 @section('content-wrapper')
     @if (isset($search))
     <section class="about home-about">
-        <div class="container">
-            <div class="title">
-                <h3>Life Goals, Dreams, Hopes, and Improvement Sections
-                    <span>
-                        "We Need Each Other"
-                    </span>
-                </h3>
-            </div>
+        <div class="container" style="margin-top:100px;">
             <div class="row gy-3">
                 @foreach ($search as $item)
                     <div class="col-md-3">
-                        <a href="./who-am-i.html">
+                        <a href="#">
                             <figure class="img-card">
-                                <figcaption style="color:black;">
-                                    {{-- <h4>{{ $item->name }}</h5>
-                                    <h5>{{ $item->address }}</h5>
-                                    <h5>{{ $item->city }}</h5>
-                                    <h5>{{ $item->state }}</h5>
-                                    <h5>{{ $item->zip_code }}</h5>
-                                    <h5>{{ $item->phone }}</h5> --}}
+                                <figcaption style="color:black; text-align:center;">
+                                    <h4>{{ $item->name }}</h4>
+                                    <span>{{ $item->address }}</span><br>
+                                    <span>{{ $item->city }}</span><br>
+                                    <span>{{ $item->state }}</span><br>
+                                    <span>{{ $item->zip_code }}</span><br>
+                                    <span>{{ $item->phone }}</span>
                                 </figcaption>
                             </figure>
                         </a>
